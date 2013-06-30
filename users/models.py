a214ce7968b85f72448f28ed4b6b542995f6ecd6
Igor Davydenko <playpauseandstop@gmail.com>
2013-06-30 18:23:32 +0300

    Add relations between user and feeds and tags with migration.

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ Implementation of user model for Chitatel project.
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
@@ -16,3 +17,16 @@ class User(AbstractUser):
     User model inherited from Django standard one, but with adding custom
     fields and relations.
     """
+    feeds = models.ManyToManyField(
+        'feeds.Feed', blank=True, null=True, related_name='users',
+        verbose_name=_('feeds')
+    )
+    tags = models.ManyToManyField(
+        'feeds.Tag', blank=True, null=True, related_name='users',
+        verbose_name=_('tags')
+    )
+
+    class Meta:
+        db_table = 'chitatel_user'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
